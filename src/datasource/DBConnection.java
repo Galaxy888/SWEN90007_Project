ba753ff2889b464c49167782052162d7634cdf68
Yu Guo <y.guo38@student.unimelb.edu.au>
2020-09-08 22:28:37 +1000

    Initialize the Project

---
+++ src/datasource/DBConnection.java
@@ -0,0 +1,52 @@
+package datasource;
+
+import java.sql.*;
+
+public class DBConnection {
+
+	private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/LMS";
+	private static final String DB_USER = "postgres";
+	private static final String DB_PASSWORD = "asdzxc123";
+
+
+	public static PreparedStatement prepare(String stm) throws SQLException {
+
+		PreparedStatement preparedStatement = null;
+		try {
+
+			Connection dbConnection = getDBConnection();
+
+			preparedStatement = dbConnection.prepareStatement(stm);
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+
+		}
+
+		return preparedStatement;
+	}
+	private static Connection getDBConnection() {
+
+
+
+		try {
+			DriverManager.registerDriver(new org.postgresql.Driver());
+
+			Connection dbConnection = DriverManager.getConnection(
+					DB_CONNECTION, DB_USER,DB_PASSWORD);
+			return dbConnection;
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+		System.out.println("Connection problem");
+		return null;
+
+	}
+
+
+}
