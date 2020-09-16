7a3e67ff52035563227e6a1ba61731da5052d68d
Yu Guo <y.guo38@student.unimelb.edu.au>
2020-09-16 22:36:07 +1000

    update index page

--- src/controllers/addSubjectController.java
+++ src/controllers/addSubjectController.java
@@ -15,7 +15,7 @@ import domain.Subject;
 /**
  * Servlet implementation class addSubjectController
  */
-@WebServlet("/")
+@WebServlet("/subject")
 public class addSubjectController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
