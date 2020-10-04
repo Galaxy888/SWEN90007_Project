dca87b2150ca23cf1578d4cc4656c4d9e1a2d83c
Yu Guo <31333514+Galaxy888@users.noreply.github.com>
2020-10-04 23:40:59 +1100

    Update DBConnection.java

--- src/datasource/DBConnection.java
+++ src/datasource/DBConnection.java
@@ -4,44 +4,25 @@ import java.sql.*;
 
 public class DBConnection {
 
-	private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/myDB";
-	private static final String DB_USER = "postgres";
-	private static final String DB_PASSWORD = "asdzxc123";
-
-
 	public static PreparedStatement prepare(String stm) throws SQLException {
-
 		PreparedStatement preparedStatement = null;
 		try {
-
 			Connection dbConnection = getDBConnection();
-
 			preparedStatement = dbConnection.prepareStatement(stm);
-
 		} catch (SQLException e) {
-
 			System.out.println(e.getMessage());
-
-
 		}
-
 		return preparedStatement;
 	}
+
 	private static Connection getDBConnection() {
-
-
-
 		try {
 			DriverManager.registerDriver(new org.postgresql.Driver());
-
-			Connection dbConnection = DriverManager.getConnection(
-					DB_CONNECTION, DB_USER,DB_PASSWORD);
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
+			Connection dbConnection = DriverManager.getConnection(dbUrl);
 			return dbConnection;
-
 		} catch (SQLException e) {
-
 			System.out.println(e.getMessage());
-
 		}
 		System.out.println("Connection problem");
 		return null;
